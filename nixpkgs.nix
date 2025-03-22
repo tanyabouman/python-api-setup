3ef7992c2052ce9e9ccafc7a70e707a67dbc9d74
Tanya Bouman <tanyabouman@gmail.com>
2025-03-22 14:00:03 -0400

    update dependencies to avoid gunicorn vulnerability

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,6 +1,6 @@
 let
-  version = "23.05";
-  sha256 = "sha256:10wn0l08j9lgqcw8177nh2ljrnxdrpri7bp0g7nvrsn9rkawvlbf";
+  version = "24.11";
+  sha256 = "sha256:1gx0hihb7kcddv5h0k7dysp2xhf1ny0aalxhjbpj2lmvj7h9g80a";
   nixpkgs = fetchTarball {
     inherit sha256;
     url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/${version}.tar.gz";
